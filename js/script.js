88470a91ea067532f770627bbb92224676ffe489
KarenPalyann <123469055+KarenPalyann@users.noreply.github.com>
2023-02-07 18:49:20 +0400

    tnain

---
+++ js/script.js
@@ -0,0 +1,61 @@
+
+
+
+
+
+
+// const red = document.querySelector('.red')
+// red.style.backgroundColor = 'black'
+// const yellow = document.querySelector('.yellow')
+// yellow.style.backgroundColor = ('black')
+// const green = document.querySelector('.green')
+// green.style.backgroundColor = ('black')
+
+// function changecolor() {
+//     const red = document.querySelector('.red')
+//     red.style.backgroundColor = 'red'
+//     yellow.style.backgroundColor = ('black')
+//     green.style.backgroundColor = ('black')
+// }
+// function changecolor1() {
+//     const red = document.querySelector('.red')
+//     yellow.style.backgroundColor = ('yellow')
+//     red.style.backgroundColor = 'black'
+//     green.style.backgroundColor = ('black')
+// }
+// function changecolor2() {
+//     const red = document.querySelector('.red')
+//     green.style.backgroundColor = ('green')
+//     yellow.style.backgroundColor = ('black')
+//    red.style.backgroundColor = 'black'  
+// }
+// setInterval(changecolor, 3000);
+// setInterval(changecolor1, 4000)
+// setInterval(changecolor2, 6000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
